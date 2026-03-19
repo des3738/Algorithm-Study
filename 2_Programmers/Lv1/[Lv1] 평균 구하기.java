a8b71957fc5c4429efbe15acf9949132d9a0a61f
des3738 <pyo9911@naver.com>
2026-03-19 21:12:24 +0900

    [Lv1] 평균 구하기 - 배열 합산

--- 2_Programmers/Lv1/[Lv1] 평균 구하기.java
+++ 2_Programmers/Lv1/[Lv1] 평균 구하기.java
@@ -1,21 +1,13 @@
 // 문제: 평균 구하기 (Lv.1)
 // 풀이: 배열 합산 후 길이로 나누기
 
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-
-// arr_len은 배열 arr의 길이입니다.
-double solution(int arr[], size_t arr_len) {
-    
-    double answer = 0;
-    double sum = 0;
-    
-    for(int i=0;i<arr_len;i++) {
-        sum += arr[i];
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        double sum = 0;
+        for (int i:arr){
+             sum += i;
+        }
+        return sum/arr.length;
     }
-    
-    answer = sum / arr_len;
-    
-    return answer;
 }
