17bcc7de64dbbd6f2aee4eb7e8930a799654b5df
des <pyo9911@naver.com>
2026-03-23 20:37:16 +0900

    [Lv1] 정수 내림차순으로 배치하기 - 문자열 변환 후 정렬

---
+++ 2_Programmers/Lv1/[Lv1] 정수 내림차순으로 배치하기.java
@@ -0,0 +1,20 @@
+// 문제: 정수 내림차순으로 배치하기
+// 풀이: 문자열 변환 후 정렬
+
+import java.util.Collections;
+import java.util.Arrays;
+
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        String str = Long.toString(n);
+        String[] arr = str.split("");
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        String s = "";
+        for(String i:arr) {
+            s += i;
+        }
+        return Long.parseLong(s);
+    }
+}
